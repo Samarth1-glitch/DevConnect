dd28d22d325a4d526e0ead040af70ed6aecf9ac6
Samarth1-glitch <nsamarth2006@gmail.com>
2025-04-21 12:52:37 +0530

    Loader.jsx

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center h-64">
+      <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-blue-500"></div>
+    </div>
+  )
+}
+
+export default Loader
